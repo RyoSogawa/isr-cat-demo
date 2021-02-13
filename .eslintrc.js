431c63ff61f16514a4973cb04586ec284115bce5
RyoSogawa <koashimitekara.1122@gmail.com>
2021-02-14 00:30:34 +0900

    [add] eslint,prettier導入

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  plugins: ['@typescript-eslint', 'react'],
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-no-target-blank': 2,
+    '@typescript-eslint/no-explicit-any': 'off',
+    'object-shorthand': ['error', 'properties'],
+    'no-console': ['error', { allow: ['warn', 'error'] }],
+  },
+}
